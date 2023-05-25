5d524703b144850a94289207230c5b6acb401239
Leo <contato@inerente.org>
2023-05-25 16:24:43 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,5 +6,5 @@ mysqldump --databases glpi10 -u root -p'senha' > /Maria/Backup/bkpglpi.sql
 #Inicia o serviço cron
 service cron restart
 
-#RUN (/opt/mssql/bin/sqlservr --accept-eula & ) | grep -q "Starting database restore" && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'iCx,74*Ary' -d root -i glpibkp.sql
-#CMD [ "cat glpibkp.sql | /usr/bin/mysql -u root -p'iCx,74*Ary' glpi10" ]
+#RUN (/opt/mssql/bin/sqlservr --accept-eula & ) | grep -q "Starting database restore" && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'senha' -d root -i glpibkp.sql
+#CMD [ "cat glpibkp.sql | /usr/bin/mysql -u root -p'senha' glpi10" ]
